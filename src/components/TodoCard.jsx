4c3d9d4f72c2e35e884c2118c1a979cbdfa4e765
Liv Clarence A. Torres <149602529+DontReb@users.noreply.github.com>
2025-09-05 21:06:29 +0800

    Added an attribute

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -8,7 +8,7 @@ export default function TodoCard(props) {
     <div className="card todo-item">
       <p>{todo.input}</p>
       <div className="todo-buttons">
-        <button>
+        <button disabled={todo.complete}>
           <h6>Done</h6>
         </button>
         <button>
